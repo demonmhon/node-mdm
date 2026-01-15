d1ecfced6d0b03d5a02995425211f342af875d48
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2026-01-16 00:04:29 +0700

    feat: add database

---
+++ migrations/V1__Create_EAV_Tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE entities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE attributes (
+    id SERIAL PRIMARY KEY,
+    entity_id INTEGER REFERENCES entities(id) ON DELETE CASCADE,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE eav_values (
+    id SERIAL PRIMARY KEY,
+    entity_id INTEGER REFERENCES entities(id) ON DELETE CASCADE,
+    attribute_id INTEGER REFERENCES attributes(id) ON DELETE CASCADE,
+    value TEXT NOT NULL
+);
